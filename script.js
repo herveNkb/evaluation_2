f51dc4e8fc1045b19a214061ac137f06458c4a0c
airV <herve.jacotet@sfr.fr>
2022-01-04 16:49:19 +0100

    create 'const' for DOM selector

---
+++ script.js
@@ -0,0 +1,11 @@
+//  sélecteurs DOM
+const rule = document.querySelector('#rule');
+const score1 = document.querySelector('#score1');
+const score2 = document.querySelector('#score2');
+const winnerLoser1 = document.querySelector('#winnerLoser1');
+const winnerLoser2 = document.querySelector('#winnerLoser2');
+const orderedReplay = document.querySelector('#replay');
+const imgDice = document.querySelector('#imgDice');
+const rollDice = document.querySelector('#rollDice');
+const pointsValue = document.querySelector('#pointsValue');
+const holdPoints = document.querySelector('holdPoints');
