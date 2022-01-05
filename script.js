b57e1002aef6ff80e51d7cf06a1e0b8457e68c0d
airV <herve.jacotet@sfr.fr>
2022-01-05 11:09:38 +0100

    Modal functional

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const holdPoints = document.querySelector('holdPoints');
 winnerLoser1.style.display = 'none';
 winnerLoser2.style.display = 'none';
 
-// ---Début pour la modal ou il y à les règles du jeu---
+// ---Début pour la modal ou il y a les règles du jeu---
 let modal = null;
 
 const openModal = (e) => {
@@ -25,18 +25,40 @@ const openModal = (e) => {
   target.setAttribute('aria-modal', 'true');
   modal = target;
   modal.addEventListener('click', closeModal);
+  modal.querySelector('.exitModal').addEventListener('click', closeModal);
+  modal
+    .querySelector('.js-modal-stop')
+    .addEventListener('click', stopPropagation);
 };
 
 const closeModal = (e) => {
   if (modal === null) return;
   e.preventDefault();
-  modal.style.display = 'none';
+  window.setTimeout(() => { // retarde la fermeture de la modal de 500 ms, ce qui permet de mettre une animation
+    modal.style.display = "none"
+    modal = null;
+  }, 500);
   modal.setAttribute('aria-hidden', 'true');
   modal.removeAttribute('aria-modal');
   modal.removeEventListener('click', closeModal);
-  modal = null;
+  modal.querySelector('.exitModal').removeEventListener('click', closeModal);
+  modal
+    .querySelector('.js-modal-stop')
+    .removeEventListener('click', stopPropagation);
+};
+
+// Fonction qui permet d'éviter de fermer la modal en cliquant ailleurs que sur le bouton "Fermer"
+const stopPropagation = (e) => {
+  e.stopPropagation();
 };
 
 document.querySelectorAll('.js-modal').forEach((a) => {
   a.addEventListener('click', openModal);
+});
+
+// Permet de se servir de la touche "Echap" du clavier pour fermer la modal
+window.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' || e.key === 'Esc') {
+    closeModal(e);
+  }
 });
